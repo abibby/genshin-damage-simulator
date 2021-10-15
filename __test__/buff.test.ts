c48b00df7a85544114f83a544225bf8c76e7fb73
Adam Bibby <adam_bibby@hotmail.com>
2021-10-15 19:58:38 -0400

    move snapshot tests to buffs file

--- __test__/buff.test.ts
+++ __test__/buff.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals'
 import { BuffCharacter, Element } from '../src/characters/character'
 import { Simulation } from '../src/simulator/simulator'
-import { basicHit, character } from './util'
+import { basicHit, character, multiHit } from './util'
 
 describe('buff', () => {
     test('base damage', () => {
@@ -23,5 +23,48 @@ describe('buff', () => {
             ['n1', '2', 'n1'],
         )
         expect(damage).toBe(15)
+    })
+
+    test('snapshot', () => {
+        const s = new Simulation()
+        const c1 = character([])
+        c1.abilities.get('n1')!.buffs[0] = {
+            frame: 0,
+            duration: 15,
+            character: BuffCharacter.All,
+            statBonuses: {
+                atk: {
+                    flat: 50,
+                    percent: 0,
+                },
+            },
+        }
+
+        const c2 = character(multiHit(Element.Pyro, 10, 2, 10))
+        c2.abilities.get('n1')!.snapshot = true
+
+        const damage = s.run([c1, c2], ['n1', '2', 'n1'])
+        expect(damage).toBe(30)
+    })
+
+    test('non snapshot', () => {
+        const s = new Simulation()
+        const c1 = character([])
+        c1.abilities.get('n1')!.buffs[0] = {
+            frame: 0,
+            duration: 15,
+            character: BuffCharacter.All,
+            statBonuses: {
+                atk: {
+                    flat: 50,
+                    percent: 0,
+                },
+            },
+        }
+
+        const c2 = character(multiHit(Element.Pyro, 10, 2, 10))
+
+        const damage = s.run([c1, c2], ['n1', '2', 'n1'])
+        expect(damage).toBe(25)
     })
 })
